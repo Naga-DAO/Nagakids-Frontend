bae2e34fbb38a39bd2993217e9e413d7c0d0a554
pleum <pleum@users.noreply.github.com>
2022-07-03 20:14:31 +0700

    fix lint

--- src/App.js
+++ src/App.js
@@ -313,7 +313,7 @@ function App () {
               ? (
                 <div>
                   <div>The sale has ended.</div>
-                  You can still find {CONFIG.NFT_NAME} on {" "}
+                  You can still find {CONFIG.NFT_NAME} on {' '}
                   <StyledLink target='_blank' href={CONFIG.MARKETPLACE_LINK}>
                     {CONFIG.MARKETPLACE}
                   </StyledLink>
